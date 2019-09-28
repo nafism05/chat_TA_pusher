969aaffa5563cc4116ce882973a8920ef2f6bf4e
Nafis <nafism05@gmail.com>
2019-09-28 14:11:28 +0700

    lokal ubuntu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     {{-- <meta name="_token" content="{!! csrf_token() !!}"> --}}
-    <meta name="csrf-token" content="{!! csrf_token() !!}">
+    <meta name="csrf-token" content="{!! csrf_token() !!}"/>
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
