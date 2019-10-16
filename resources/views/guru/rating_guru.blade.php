0af5e1e56158a85b28f25857d34708a3403240d3
Nafis <nafism05@gmail.com>
2019-10-16 21:18:33 +0700

    .

--- resources/views/guru/rating_guru.blade.php
+++ resources/views/guru/rating_guru.blade.php
@@ -13,11 +13,11 @@
 	    <div class="row">
 	    	<div class="col-11 offset-1">
 				<p>Rating Anda : {{ $data['rating'] }}</p>
-				<span class="fa fa-star {{ $data['rfloor'] >= 1 ? 'checked' : '' }}"></span>
-				<span class="fa fa-star {{ $data['rfloor'] >= 2 ? 'checked' : '' }}"></span>
-				<span class="fa fa-star {{ $data['rfloor'] >= 3 ? 'checked' : '' }}"></span>
-				<span class="fa fa-star {{ $data['rfloor'] >= 4 ? 'checked' : '' }}"></span>
-				<span class="fa fa-star {{ $data['rfloor'] >= 5 ? 'checked' : '' }}"></span>
+				<span class="fa fa-star faguru {{ $data['rfloor'] >= 1 ? 'checked' : '' }}"></span>
+				<span class="fa fa-star faguru {{ $data['rfloor'] >= 2 ? 'checked' : '' }}"></span>
+				<span class="fa fa-star faguru {{ $data['rfloor'] >= 3 ? 'checked' : '' }}"></span>
+				<span class="fa fa-star faguru {{ $data['rfloor'] >= 4 ? 'checked' : '' }}"></span>
+				<span class="fa fa-star faguru {{ $data['rfloor'] >= 5 ? 'checked' : '' }}"></span>
 	    	</div>
 	    </div>
 	</div>
